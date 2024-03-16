224bead0bb9e918589dcb20cbd6682d7aba9a36f
vsharika1 <vsh1411@gmail.com>
2024-03-15 21:07:09 -0700

    Updated NavDrawer and added NavItems

--- app/components/NavDrawer.tsx
+++ app/components/NavDrawer.tsx
@@ -1,14 +1,5 @@
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import AssessmentIcon from '@mui/icons-material/Assessment';
-import BadgeIcon from '@mui/icons-material/Badge';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import EventAvailableIcon from '@mui/icons-material/EventAvailable';
-import EventBusyIcon from '@mui/icons-material/EventBusy';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import ScheduleIcon from '@mui/icons-material/Schedule';
-import SettingsIcon from '@mui/icons-material/Settings';
-import SwapHorizontalCircleIcon from '@mui/icons-material/SwapHorizontalCircle';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
@@ -17,6 +8,8 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 
+import { ManagerNavItems, EmployeeNavItems } from './NavItems';
+
 const drawerWidth = 300;
 
 interface Props {
@@ -24,25 +17,6 @@ interface Props {
 }
 
 export default function NavDrawer({ isManager }: Props) {
-  const managerItems = [
-    { text: 'Dashboard', icon: <DashboardIcon /> },
-    { text: 'Schedule Management', icon: <ScheduleIcon /> },
-    { text: 'Employee Management', icon: <BadgeIcon /> },
-    { text: 'Shift Swap Approvals', icon: <SwapHorizontalCircleIcon /> },
-    { text: 'Notifications', icon: <NotificationsIcon /> },
-    { text: 'Reports', icon: <AssessmentIcon /> },
-    { text: 'Settings', icon: <SettingsIcon /> },
-  ];
-
-  const employeeItems = [
-    { text: 'My Schedule', icon: <ScheduleIcon /> },
-    { text: 'Available Shifts', icon: <BadgeIcon /> },
-    { text: 'Shift Swap', icon: <SwapHorizontalCircleIcon /> },
-    { text: 'Availability', icon: <EventAvailableIcon /> },
-    { text: 'Request Time Off', icon: <EventBusyIcon /> },
-    { text: 'Notifications', icon: <NotificationsIcon /> },
-    { text: 'Settings', icon: <SettingsIcon /> },
-  ];
 
   return (
     <Drawer
@@ -66,17 +40,10 @@ export default function NavDrawer({ isManager }: Props) {
       </div>
       <Divider />
       <List>
-        {(isManager ? managerItems : employeeItems).map((item) => (
-          <ListItem button key={item.text}>
-            <ListItemIcon sx={{ color: 'white' }}>{item.icon}</ListItemIcon>
-            <ListItemText primary={item.text} />
-          </ListItem>
-        ))}
+        {isManager ? ManagerNavItems : EmployeeNavItems}
       </List>
       <Divider />
-
       <List sx={{ marginTop: 'auto', paddingBottom: 2 }}>
-        <Divider />
         {/* Profile Button */}
         <ListItem button>
           <ListItemIcon sx={{ color: 'white' }}>
@@ -84,7 +51,6 @@ export default function NavDrawer({ isManager }: Props) {
           </ListItemIcon>
           <ListItemText primary="Profile" />
         </ListItem>
-
         {/* Log Out Button */}
         <ListItem button>
           <ListItemIcon sx={{ color: 'white' }}>
