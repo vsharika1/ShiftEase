cfd9a8459df41311b4e627e8afde8f38980066b0
fire332 <96039230+fire332@users.noreply.github.com>
2024-03-21 03:01:49 -0700

    fix GAE deployment

--- app/.server/db.ts
+++ app/.server/db.ts
@@ -1,4 +1,15 @@
-// import { PrismaClient } from '@prisma/client';
-import { PrismaClient } from '../generated/client';
+import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+let datasourceUrl: string | undefined;
+
+if (process.env.INSTANCE_UNIX_SOCKET) {
+  if (!process.env.DB_PASS) throw new Error('DB_PASS is required');
+  const source = new URL('postgresql://localhost/postgres');
+  source.username = 'postgres';
+  source.password = process.env.DB_PASS;
+  source.searchParams.set('host', process.env.INSTANCE_UNIX_SOCKET);
+
+  datasourceUrl = source.href;
+}
+
+export const prisma = new PrismaClient({ datasourceUrl });
